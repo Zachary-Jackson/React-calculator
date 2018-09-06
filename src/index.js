7de56f0014e9b20223a293ead718ab2090d0ea77
Zachary R Jackson <jackson.zachary.r@gmail.com>
2018-09-06 17:17:21 -0400

    Release 1.0 Working Calculator
    The calculator now adds, multiplies, divides, and subtracts properly and updates the display.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,74 @@ const rootElement = document.getElementById("root");
 // state is the primary information holder for App
 const state = { screenDisplay: "", answer: "n/a" };
 
+function createAnswer(equation) {
+  /**
+   * Calculates an answer based on an equation
+   *
+   * :param equation: A string version of an equation
+   * :return: The new calculated equaiton in string format
+   */
+
+  let wordList = equation.split(/[ ,]+/).filter(Boolean);
+
+  // We need to do order of operations, which means multiplication,
+  // division, addition, subtraction
+  let i;
+
+  // May need to run this for loop multiple times
+  while (wordList.includes("x")) {
+    for (i = 0; i < wordList.length; i++) {
+      if (wordList[i] == "x") {
+        const newValue = wordList[i - 1] * wordList[i + 1];
+        wordList[i] = newValue;
+        delete wordList[i + 1];
+        delete wordList[i - 1];
+        wordList = wordList.filter(e => e !== undefined);
+      }
+    }
+  }
+
+  while (wordList.includes("/")) {
+    for (i = 0; i < wordList.length; i++) {
+      if (wordList[i] == "/") {
+        const newValue = wordList[i - 1] / wordList[i + 1];
+        wordList[i] = newValue;
+        delete wordList[i + 1];
+        delete wordList[i - 1];
+        wordList = wordList.filter(e => e !== undefined);
+      }
+    }
+  }
+
+  while (wordList.includes("+")) {
+    for (i = 0; i < wordList.length; i++) {
+      if (wordList[i] == "+") {
+        const newValue =
+          parseFloat(wordList[i - 1]) + parseFloat(wordList[i + 1]);
+        wordList[i] = newValue;
+        delete wordList[i + 1];
+        delete wordList[i - 1];
+        wordList = wordList.filter(e => e !== undefined);
+      }
+    }
+  }
+
+  while (wordList.includes("-")) {
+    for (i = 0; i < wordList.length; i++) {
+      if (wordList[i] == "-") {
+        const newValue =
+          parseFloat(wordList[i - 1]) - parseFloat(wordList[i + 1]);
+        wordList[i] = newValue;
+        delete wordList[i + 1];
+        delete wordList[i - 1];
+        wordList = wordList.filter(e => e !== undefined);
+      }
+    }
+  }
+
+  return wordList;
+}
+
 function digitHandler(oldString, input) {
   /**
    * Checks to see if the screenDisplay is empty
@@ -44,7 +112,7 @@ function newInputHandler(event) {
    * :param event: onClick event
    */
   const input = event.currentTarget.textContent;
-  const isInt = parseInt(input);
+  const isInt = parseFloat(input);
 
   if (isInt || isInt === 0 || input === ".") {
     state.screenDisplay = digitHandler(state.screenDisplay, input);
@@ -54,6 +122,9 @@ function newInputHandler(event) {
     /* If the input is "clear" clear the display*/
     state.screenDisplay = "";
   }
+
+  // Calculates and changes the calculator answer
+  state.answer = updateAnswer(state.screenDisplay);
 
   renderApp();
 }
@@ -75,7 +146,7 @@ function operatorHandler(oldString, input) {
   const wordList = oldString.split(/[ ,]+/).filter(Boolean);
   const lastWord = wordList.slice(-1)[0];
 
-  const operators = ["%", "x", "-", "+"];
+  const operators = ["/", "x", "-", "+"];
 
   if (operators.includes(lastWord)) {
     return oldString;
@@ -85,6 +156,34 @@ function operatorHandler(oldString, input) {
     return oldString + " " + input + " ";
   }
   return "";
+}
+
+function updateAnswer(display) {
+  /**
+   * Uses display to see if an answer can be shown
+   *
+   * :param display: A string value in the format of a calculator screen
+   * :return if applicable: The new calculated ansewr
+   * "return non-applicable: n/a"
+   */
+  if (display === "") {
+    return "n/a";
+  }
+
+  // Checks to see if the last value is an operator or ends with a period
+  const wordList = display.split(/[ ,]+/).filter(Boolean);
+  const lastWord = wordList.slice(-1)[0];
+
+  const operators = ["%", "x", "-", "+"];
+
+  if (operators.includes(lastWord)) {
+    return "n/a";
+  } else if (lastWord.slice(-1)[0] === ".") {
+    return "n/a";
+  }
+
+  // The display can create a new answer
+  return createAnswer(display);
 }
 
 function App() {
@@ -105,7 +204,7 @@ function App() {
           9
         </button>
         <button className="large-button" onClick={newInputHandler}>
-          %
+          /
         </button>
       </div>
 
