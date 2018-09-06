0f26a31950723a2acbb503ec69dbe238bd1bf5fe
Zachary R Jackson <jackson.zachary.r@gmail.com>
2018-09-05 22:56:25 -0400

    Added display logic
    The display logic for showing the user's inputs to the calculator screen has been added. They can clear the screen as well.

--- src/index.js
+++ src/index.js
@@ -6,45 +6,118 @@ import "./styles.css";
 const rootElement = document.getElementById("root");
 
 // state is the primary information holder for App
-const state = { screenDisplay: "9 x 9 + 1", answer: "82" };
+const state = { screenDisplay: "", answer: "n/a" };
+
+function newInputHandler(event) {
+  /**
+   * Determines what to do with a click event.
+   * Then calls the appropriate functions
+   *
+   * :param event: onClick event
+   */
+  const input = event.currentTarget.textContent;
+  const isInt = parseInt(input);
+
+  if (isInt || isInt === 0) {
+    state.screenDisplay += input;
+  } else if (input !== "clear") {
+    state.screenDisplay = operatorHandler(state.screenDisplay, input);
+  } else {
+    state.screenDisplay = "";
+  }
+
+  renderApp();
+}
+
+function operatorHandler(oldString, input) {
+  /**
+   * Checks to see if the screenDisplay is empty
+   * If so deny the user's operator request
+   *
+   * :param oldString: The old string that is checked for truthiness
+   * :param input: A string used to create the return string
+   * :return if valid: Str in the format of space + input + space
+   * : return invalid: Str that is empty ""
+   */
+  if (oldString) {
+    return oldString + " " + input + " ";
+  }
+  return "";
+}
 
 function App() {
   return (
-    <div class="center">
-      <h1 class="center box">
+    <div className="center">
+      <h1 className="center box">
         {state.screenDisplay} = {state.answer}
       </h1>
 
       <div>
-        <button class="large-button">7</button>
-        <button class="large-button">8</button>
-        <button class="large-button">9</button>
-        <button class="large-button">%</button>
+        <button className="large-button" onClick={newInputHandler}>
+          7
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          8
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          9
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          %
+        </button>
       </div>
 
       <div>
-        <button class="large-button">4</button>
-        <button class="large-button">5</button>
-        <button class="large-button">6</button>
-        <button class="large-button">X</button>
+        <button className="large-button" onClick={newInputHandler}>
+          4
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          5
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          6
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          x
+        </button>
       </div>
 
       <div>
-        <button class="large-button">1</button>
-        <button class="large-button">2</button>
-        <button class="large-button">3</button>
-        <button class="large-button">-</button>
+        <button className="large-button" onClick={newInputHandler}>
+          1
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          2
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          3
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          -
+        </button>
       </div>
+
       <div>
-        <button class="large-button">0</button>
-        <button class="large-button">.</button>
-        <button class="large-button">=</button>
-        <button class="large-button">+</button>
+        <button className="large-button" onClick={newInputHandler}>
+          0
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          .
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          clear
+        </button>
+        <button className="large-button" onClick={newInputHandler}>
+          +
+        </button>
       </div>
     </div>
   );
 }
 function renderApp() {
+  /**
+   * Calls a ReactDom render using <App> on the "root" id
+   */
   ReactDOM.render(<App />, document.getElementById("root"));
 }
 
