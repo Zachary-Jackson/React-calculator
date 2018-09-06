90ee7de2fa9e1eb47781928ff43bc5e354dfb728
Zachary R Jackson <jackson.zachary.r@gmail.com>
2018-09-06 15:26:43 -0400

    Input edge cases handled
    Invalid inputs like "877.98.98" or "5 x x 4" are no longer allowed.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,34 @@ const rootElement = document.getElementById("root");
 // state is the primary information holder for App
 const state = { screenDisplay: "", answer: "n/a" };
 
+function digitHandler(oldString, input) {
+  /**
+   * Checks to see if the screenDisplay is empty
+   * If so deny the user's operator request
+   *
+   * :param oldString: The old string that is checked to see if there
+   * is an ending period
+   * :param input: A string version of a number or period
+   * :return if valid: Str in the format of space + input + space
+   * : return  if invalid: Str that is the same as oldString
+   */
+
+  if (input !== ".") {
+    return oldString + input;
+  }
+
+  // if input is not a number check if we can place a decimal
+  const wordList = oldString.split(/[ ,]+/);
+  const lastWord = wordList.slice(-1)[0];
+
+  // If the last word already contains a decimal block operation
+  if (lastWord.includes(".") === false) {
+    return oldString + input;
+  } else {
+    return oldString;
+  }
+}
+
 function newInputHandler(event) {
   /**
    * Determines what to do with a click event.
@@ -18,11 +46,12 @@ function newInputHandler(event) {
   const input = event.currentTarget.textContent;
   const isInt = parseInt(input);
 
-  if (isInt || isInt === 0) {
-    state.screenDisplay += input;
+  if (isInt || isInt === 0 || input === ".") {
+    state.screenDisplay = digitHandler(state.screenDisplay, input);
   } else if (input !== "clear") {
     state.screenDisplay = operatorHandler(state.screenDisplay, input);
   } else {
+    /* If the input is "clear" clear the display*/
     state.screenDisplay = "";
   }
 
@@ -39,6 +68,19 @@ function operatorHandler(oldString, input) {
    * :return if valid: Str in the format of space + input + space
    * : return invalid: Str that is empty ""
    */
+
+  // First check if the last word is an operator
+  // If so ignore
+  // if input is not a number check if we can place a decimal
+  const wordList = oldString.split(/[ ,]+/).filter(Boolean);
+  const lastWord = wordList.slice(-1)[0];
+
+  const operators = ["%", "x", "-", "+"];
+
+  if (operators.includes(lastWord)) {
+    return oldString;
+  }
+
   if (oldString) {
     return oldString + " " + input + " ";
   }
